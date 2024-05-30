ca32fd1509a667c222243e283be48d9bd7fce5bd
Sra Suzuki <alesuzuki@gmail.com>
2024-05-30 19:59:37 -0300

    Criando um Sistema e Abstraindo um Celular com POO
    Desafio: Criando um Sistema e Abstraindo um Celular com POO em C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia("11999996666", "N95", "777666555444321", 32);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Sbrubles");
+
+Console.WriteLine("__________________________________________");
+
+Console.WriteLine("iPhone");
+Smartphone iphone = new Iphone("+5511988885555", "Caro", "321321321654654654", 64);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Twitch");
